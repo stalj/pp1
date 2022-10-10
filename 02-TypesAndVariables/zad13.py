8edf800d6e0f99928b51e991a0c9643032444283
Jan Doniec <john@techmaxi.net>
2022-10-10 19:06:39 +0200

    Merge pull request #1 from jandoniec/main

---
+++ 02-TypesAndVariables/zad13.py
@@ -0,0 +1,21 @@
+from math import pi
+
+
+r=5
+pinumber=3.14
+
+#area with accurate pi version
+P=(5**2)*pi
+print(P)
+
+#area with defined pi
+P1=(5**2)*pinumber
+print(P1)
+
+#circumference with accurate pi version
+C=2*pi*5
+print(C)
+
+#circumference with defined pi
+C1=2*pinumber*5
+print(C1)
