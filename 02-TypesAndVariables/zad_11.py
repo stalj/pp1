3d0de67371724306178a3a8430b082043bacf4b8
WiktoriaSadlo <227960@student.uek.krakow.pl>
2022-10-13 08:24:31 +0200

    zmiana11

--- 02-TypesAndVariables/zad_11.py
+++ 02-TypesAndVariables/zad_11.py
@@ -1,1 +1,10 @@
-n1=
+imie= input("Podaj swoje imię:")
+wiek=input("Podaj swój wiek:")
+wzrost=input("Podaj  swój wzrost (w cm):")
+
+zdanie=f"Mam na imię {imie}, mam {wiek} lat i mam {wzrost} cm wzrostu"
+
+print(zdanie)
+
+
+
