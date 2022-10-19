e80f18e010cdb434182210c8a1f835c043a39d06
Jan Doniec <john@techmaxi.net>
2022-10-19 15:14:16 +0200

    lesson03homework-2

--- 03-ControlStructures/zad18.py
+++ 03-ControlStructures/zad18.py
@@ -1,14 +1,10 @@
-wartosc=int(input("Podaj kwote:"))
-
-coin5=0
-coin2=0
-coin1=0
-
-
-if wartosc%5==0:
-    coin5=wartosc//5
-elif wartosc%2==0:
-    coin2=wartosc//2
-elif wartosc%1==0:
-    coin1=wartosc//1
-print(coin5, coin2, coin1)
+a=[1,2,5]
+b=int(input("Enter the amount: "))
+c=0
+x=2
+while(x>=0):
+    c=b//a[x]
+    b-=(c*a[x])
+    if(c>0):
+        print(f"{c} time {a[x]} PLN")
+    x=x-1
