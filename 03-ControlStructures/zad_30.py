a6c676cd51177fd8ff98b0861af94c6bc76d5044
WiktoriaSadlo <227960@student.uek.krakow.pl>
2022-10-23 23:17:41 +0200

    i

--- 03-ControlStructures/zad_30.py
+++ 03-ControlStructures/zad_30.py
@@ -1,13 +1,11 @@
-x=int(input("Podaj liczbę:"))
-z=2
-for y in range(x):
-
-    while z//(z-1):
-        x2=3
-        x3=z//x2
-        x2+=1
-    if x3==0:
-        print(z)
-
-print("gotowe")
-
+a = int(input("Podaj ilość liczb pierwszych: "))
+count=0
+i=2
+while(count!=a):
+    if i==2 or i==3 or i==5:
+        print(i)
+        count+=1
+    elif i%2!=0 and i%3!=0 and i%5!=0:
+        print(i)
+        count+=1
+    i+=1
