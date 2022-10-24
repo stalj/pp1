f8e77cbdf73f046dbf998679c2c9ff9e7683769c
WiktoriaSadlo <227960@student.uk.krakow.pl>
2022-10-24 11:12:55 +0200

    zadanka

---
+++ 04-Subroutines/zad_05.py
@@ -0,0 +1,7 @@
+def display_uni_address():
+    print("Cracow University of Economics")
+    print("Rakowicka 27")
+    print("31-510 Kraków, POLAND")
+    
+display_uni_address()
+display_uni_address()
