cbc97c3fb55225a94feb4c437de871f37d267d19
s-013-18 <s-013-18@gumiforest.ae.krakow.pl>
2022-10-26 11:08:36 +0200

    lesson 2

---
+++ 04-Subroutines/task15.py
@@ -0,0 +1,9 @@
+
+import module
+x=module.number()
+y= module.randomnumber()
+if x==y:
+    print("Bingo!")
+else:
+    print("try again")
+    
---
+++ 04-Subroutines/task8.py
@@ -0,0 +1,7 @@
+def intnum(n):
+    for i in range(1,n+1):
+        print(i, end=" ")
+
+intnum(15)
+print()
+intnum(25)
