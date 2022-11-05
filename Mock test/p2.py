f00e68eb8ed7302d03816c4f048e43adb108f256
WiktoriaSadlo <227960@student.uek.krakow.pl>
2022-11-05 16:45:13 +0100

    zadanka

--- Mock test/p2.py
+++ Mock test/p2.py
@@ -1,7 +1,6 @@
 def f(binary_number):
     for i in binary_number:
         if i!="1" and i!="0":
-            print("False")
-            break
+            return print("False")
     if i=="0" or i=="1":
         return print("True")
