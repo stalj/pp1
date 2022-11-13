6b6f876897a7677b7331d9a79482e9eaffaa24d0
WiktoriaSadlo <227960@student.uek.krakow.pl>
2022-11-13 23:12:22 +0100

    zadanka

--- 06-Arrays/zad_05.py
+++ 06-Arrays/zad_05.py
@@ -1,1 +1,17 @@
 import array
+import random
+
+arr1 = [3,7,1,0,4]
+arr2 = [[2,3],[7,1],[0,4]]
+arr3 = [7 for i in range(5)]
+arr4 = [i for i in range(1,10)]
+arr5 = [i*2 for i in range(1,10)]
+arr6 = [random.randint(1,20) for i in range(10)]
+arr7 = [[] for i in range(5)]
+arr8 = [[1 for i in range(2)] for j in range(4)]
+arr9 = [[random.randint(1,20) for i in range(3)] for j in range(5)]
+arr10=[0 for x in range(50)]
+arr_m=[[random.randint(0,1)] for x in range(20)]
+arr_n=[ [False for x in range(5)], [False for x in range(5)] ]
+
+print(arr_n)
