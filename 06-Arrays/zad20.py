c10ff4ab9da1b6e2185aaade2aef184f4ca43e85
Jan Doniec <john@techmaxi.net>
2022-11-16 23:41:58 +0100

    lesson05homework-2 pls help im tired

---
+++ 06-Arrays/zad20.py
@@ -0,0 +1,5 @@
+import numpy as np
+x=np.array([12, 6, 4, 9, 10])
+y=[]
+for i in x:
+    print('*'*i)
---
+++ 06-Arrays/zad25.py
@@ -0,0 +1,8 @@
+x=[1,2,3,4,5]
+
+def occurs(number, array):
+    if number in array:
+        print("in array")
+    else:
+        print("not in array")
+occurs(7,x)
