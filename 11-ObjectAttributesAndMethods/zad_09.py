ddf26154d1642414f1d327d26c797cda16c54fac
WiktoriaSadlo <227960@student.uek.krakow.pl>
2022-12-19 11:04:35 +0100

    zadanka

---
+++ 11-ObjectAttributesAndMethods/zad_09.py
@@ -0,0 +1,19 @@
+import  random
+class Arrays():
+    array=[]
+
+    @staticmethod
+    def fa(len)
+    for i in range(len):
+        array.append()
+
+    @staticmethod
+    def fb(lenght,x,y):
+        for z in range(lenght):
+            Arrays.array.append(random.randrange(x,y))
+        return Arrays.array
+
+a=Arrays.fb(3,6,9)
+print(a)
+
+    
