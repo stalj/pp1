69be704a7984a345b9ba679ab43fe9977ce5244a
s-013-19 <s-013-19@gumiforest.ae.krakow.pl>
2022-12-21 11:13:37 +0100

    add

---
+++ 11-ObjectAttributesAndMethods/task9.py
@@ -0,0 +1,11 @@
+class Arrays():
+    list= []
+    @staticmethod
+    def creating_array(number_of_array_elements, value_of_array_elements):
+        for i in number_of_array_elements:
+            if i< number_of_array_elements:
+                list.append(value_of_array_elements)
+        
+    @staticmethod
+    def c_array(number_of_array_elements, value_from, value_to):
+   
